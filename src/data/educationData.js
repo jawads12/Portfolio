7d32f686cbf405d3e4124fb83c57bb495e56b718
Jawad <jawadrahman483@gmail.com>
2023-10-29 15:24:05 +0600

    up

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,23 +1,23 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'Ramjas College, Delhi University',
-        course: 'B.A. Honours History',
-        startYear: '2015',
-        endYear: '2018'
+        institution: 'Military Institute of Science and Technology, Dhaka',
+        course: 'BSc Engineering in CSE',
+        startYear: '2021',
+        endYear: 'Present'
     },
     {
         id: 2,
-        institution: 'Delhi University',
-        course: 'M.A. History',
-        startYear: '2019',
-        endYear: '2021'
+        institution: 'Hazi Muhammad Mohsin College, Chittagong',
+        course: 'HSC',
+        startYear: '2018',
+        endYear: '2020'
     },
     {
         id: 3,
-        institution: 'Masai School of Coding',
-        course: 'Fullstack Web-Development',
-        startYear: '2021',
-        endYear: 'Present'
+        institution: 'Nasirabad Govt. High School',
+        course: 'SSC',
+        startYear: '2012',
+        endYear: '2018'
     },
 ]
