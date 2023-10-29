8d5ad7b3093cffd3f4590c9ad49352946a64b9bd
Jawad <jawadrahman483@gmail.com>
2023-10-29 09:09:54 +0600

    .

---
+++ src/data/headerData.js
@@ -0,0 +1,9 @@
+import resume from '../assets/pdf/resume.pdf'
+
+export const headerData = {
+    name: 'Md Jawadur Rahman',
+    title: "I am",
+    desciption:"Life may be a meandering path but don't worry about it, rather enjoy it. It is about the journey, not the destination so explore all that comes your way, learn and grow from it, for this journey only happens once.",
+    image: 'https://drive.google.com/file/d/1af9D3wtJDHC2sF7DLZXAb2E3TrkRx0FQ/view?usp=sharing',
+    resumePdf: resume
+}
